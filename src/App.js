46901898f2ccdee4840f5b114aca88f5686f0d3c
SUMIT <sumit@SUMITs-MacBook-Air.local>
2022-10-05 17:23:10 +0530

    LoginPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail" element={<Detail />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -29,7 +29,7 @@ const Detail = () => {
           <img src="/images/group-icon.png" />
         </GroupWatchButton>
       </Controls>
-      <SubTitle>2018 • 7m •Family, Fantasy, Kids, Animation</SubTitle>
+      <SubTitle>2018 • 7m • Family, Fantasy, Kids, Animation</SubTitle>
       <Description>
         A crime mystery thriller fantasy about a man and woman sharing same
         emotions. Gye Hoon is a chef whose twin sister went missing. Now he
@@ -66,7 +66,8 @@ const ImageTitle = styled.div`
   min-height: 170px;
   width: 35vw;
   min-width: 200px;
-
+  margin-top: 60px;
+  margin-bottom: 10px;
   img {
     width: 100%;
     height: 100%;
@@ -133,4 +134,5 @@ const Description = styled.div`
   font-size: 20px;
   margin-top: 16px;
   color: rgb(249, 249, 249);
+  max-width: 700px;
 `;
